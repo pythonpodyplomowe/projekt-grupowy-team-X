fc7b6f5752092830d45a4af403295b866294054b
pythonpodyplomowe <pythonpodyplomowe@gmail.com>
2024-03-08 19:04:18 +0100

    Update for age >50 for program

---
+++ README.md
@@ -0,0 +1,9 @@
+v.1.0
+README:
+# Aplikacja dla osób dorosłych - zakup alkoholu w Pythonie
+
+## Funkcjonalności aplikacji w wersji:
+
+v.1.0
+- [x] obsługa osób dorosłych
+- [ ] obsługa wymagań specjalnych dla osób +50 [to be tested]
---
+++ program.py
@@ -0,0 +1,14 @@
+# v.1.0 dzialajacej aplikacji
+
+wiek = input("Podaj wiek uzytkownika: ")
+#sprawdzenie czy wiek jest liczba calkowitą
+if wiek.isdigit() == False:
+    exit("Wiek musi byc liczba calkowita. Zamykam aplikację")
+wiek=int(wiek)
+if wiek>=18 and wiek<=50:
+    print("Witamy w apce. Mozesz kupować u nas energetyki")
+elif:
+    print("Witamy w apce. Mozesz kupować u nas energetyki")
+    print("Uwaga: Uwazaj na serce przy spozywaniu takich napojow")
+else:
+    exit("Jestes za mlody/a na energetyki. Zapraszamy na disney.com:) ")
